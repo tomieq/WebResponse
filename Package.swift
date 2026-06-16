bd30c6773c7ced3c7211c73e0b6bfb6b7beb60a2
tomieq <tomieq@wp.pl>
2026-06-16 15:40:55 +0200

    Added support for iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WebResponse",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15), .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
